0e36a7f495b346564168a29d89549764210ee8da
Husen Abdallah <Husen.ab1015@gmail.com>
2023-09-11 14:09:46 +0300

    added fadeout time to screen fader

--- Assets/Scripts/ScreenFader.cs
+++ Assets/Scripts/ScreenFader.cs
@@ -8,11 +8,7 @@ public class ScreenFader : MonoBehaviour
 {
     public static ScreenFader Instance { get; private set; }
 
-<<<<<<< HEAD
     public float fadeTime = 0.5f;
-=======
-    public float fadeTime = 1000000f;
->>>>>>> 4899888956fa06c455ae4d23fd039bf8fec27b81
     public Color fadeColor;
     private Renderer rend;
     private void Awake()
@@ -37,11 +33,11 @@ public class ScreenFader : MonoBehaviour
     }
     IEnumerator FadeIn()
     {
-        float t = fadeTime;
+        float t = 1f;
         while (t > 0f)
         {
             t -= Time.deltaTime;
-            rend.material.SetColor("_Color", new Color(0f, 0f, 0f, t / 2));
+            rend.material.SetColor("_Color", new Color(0f, 0f, 0f, t));
             //img.color = new Color(0f, 0f, 0f, t);
             yield return 0; //wait a frame and continue
 
@@ -50,15 +46,10 @@ public class ScreenFader : MonoBehaviour
     IEnumerator FadeOut(int sceneId)
     {
         float t = 0f;
-        while (t < fadeTime)
+        while (t < 1f)
         {
-<<<<<<< HEAD
             t += Time.deltaTime * fadeTime;
             rend.material.SetColor("_Color", new Color(0f, 0f, 0f, t));
-=======
-            t += Time.deltaTime;
-            rend.material.SetColor("_Color", new Color(0f, 0f, 0f, t / 2));
->>>>>>> 4899888956fa06c455ae4d23fd039bf8fec27b81
 
             //img.color = new Color(0f, 0f, 0f, t);
             yield return 0; //wait a frame and continue
