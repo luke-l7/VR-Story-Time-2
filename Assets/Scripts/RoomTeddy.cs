23a0ded81cface5bf264fd2bd309606d5d65c641
Lucas Lucia <101108230+luke-l7@users.noreply.github.com>
2023-09-13 16:45:02 +0300

    Added sound for the Teddybear

--- Assets/Scripts/RoomTeddy.cs
+++ Assets/Scripts/RoomTeddy.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RoomTeddy : MonoBehaviour
 {
-    public Animator animator;
+    private Animator animator;
     private string bookHint1 = "Dont you think that book over by the window looks suspicios?";
     private string bookHint2 = "Maybe try extending your hand to it?";
-    private string bookCheer = "        How Magical!";
+    private string bookCheer = "How Magical!";
     public static RoomTeddy Instance { get; private set; }
     public PopupSystem popupSystem;
     private void Awake()
@@ -24,22 +24,25 @@ public class RoomTeddy : MonoBehaviour
 
     void Start()
     {
-        animator= GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     public void StandUp()
     {
         animator.SetTrigger("StandUp");
+        FMODUnity.RuntimeManager.PlayOneShot("event:/teddy_bear_hint_1");
         popupSystem.ShowPopUp(bookHint1);
     }
 
     public void GiveAnotherHint()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/teddy_bear_hint_2");
         popupSystem.ShowPopUp(bookHint2);
     }
 
     public void CheerSpeech() // changes teddy popup to a cheer
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/teddy_bear_cheer");
         popupSystem.ShowPopUp(bookCheer);
     }
 }
