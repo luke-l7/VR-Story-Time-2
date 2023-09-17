e8cec0c8b1986519dda68e77023f4ccfe462b1a2
husen1015 <husen.ab1015@gmail.com>
2023-09-17 11:43:08 +0200

    added fire that works with vr, when player hovers over it fire lights

--- Assets/Fireplace.cs
+++ Assets/Fireplace.cs
@@ -6,25 +6,38 @@ using UnityEngine;
 public class Fireplace : MonoBehaviour
 {
     public bool lightFire = false;
-    public GameObject Fire;
-
-
+    //public GameObject Fire;
+    public ParticleSystem fire;
+    private void Start()
+    {
+    }
     // Update is called once per frame
     void Update()
     {
         if (!lightFire)
         {
-            Fire.SetActive(false);
-
+            fire.GetComponent<ParticleSystem>().Stop();
+            //fire.Stop();
         }
         else
         {
-            Fire.SetActive(true);
+            fire.GetComponent<ParticleSystem>().Play();
 
+            //fire.Play();
         }
     }
     public void ToggleFire()
     {
-        Fire.SetActive(lightFire);
+        lightFire = !lightFire;
+        if (lightFire)
+        {
+            fire.GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            fire.GetComponent<ParticleSystem>().Stop();
+
+        }
     }
+
 }
